75adec4a3a63cffede0da9508bc9994dd904f54d
Ciprian <popescu.ciprian98@gmail.com>
2020-02-24 12:50:14 +0200

    human

---
+++ src/Human.java
@@ -0,0 +1,31 @@
+public class Human implements Runner {
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Human(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int getSpeed() {
+        if (this.age < 50)
+                return 10;
+        else
+            return 7;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "age=" + age +
+                '}';
+    }
+}
